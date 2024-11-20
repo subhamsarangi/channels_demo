27f4095d4ed555ce3c1b1adc9faf3e958550ebe0
subhamivan <ivan.sarangi@gmail.com>
2024-11-20 19:29:00 +0530

    chat

--- apps/chat/consumers.py
+++ apps/chat/consumers.py
@@ -6,7 +6,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
-
+        print(self.room_group_name, "0000000000")
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
